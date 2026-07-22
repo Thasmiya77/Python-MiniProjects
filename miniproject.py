091eb5ede479c16487b943f15c08b267f3097ce2
Thasmiya <galithasmiya@gmail.com>
2026-07-22 20:18:53 +0530

    Add files via upload

---
+++ miniproject.py
@@ -0,0 +1,12 @@
+import random
+target=random.randint(1,100)
+while True:
+    user_choice=int(input("pick a number:"))
+    if (target==user_choice):
+        print("your guess is right!\n congragulation!!")
+        break
+    elif (user_choice<target):
+        print("oops! your guess is wrong\n your guess is less than target")
+    else:
+            print("oops! your guess is wrong\n your guess is greater than target")  
+print(target)              
