9b9d037a1c917da69fa8c04ce6260d242e6b552a
Thasmiya <galithasmiya@gmail.com>
2026-07-22 21:56:28 +0530

    Add files via upload
    A simple Python number guessing game using the random module,loops and conditional statements

---
+++ Guess-the-Number/miniproject.py
@@ -0,0 +1,15 @@
+      
+import random
+target=random.randint(1,100)
+while True:
+    user_choice=int(input("pick a number OR Quit:"))
+    if(user_choice==quit):
+        break
+    if (target==user_choice):
+        print("your guess is right!\n congragulation!!")
+        break
+    elif (user_choice<target):
+        print("oops! your guess is wrong\n your guess is less than target")
+    else:
+            print("oops! your guess is wrong\n your guess is greater than target")  
+print("----------GAME OVER----------")              
